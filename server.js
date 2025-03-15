7bccf320c5f93cfec77db519ffb42e99faa1363f
nguyenphuochuyy <phuochuy10023@gmail.com>
2025-03-15 23:41:50 +0700

    Fix CORS policy for new FE domain

--- server.js
+++ server.js
@@ -10,10 +10,24 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const allowedOrigins = [
+  'https://expense-tracker-client-y3u9.onrender.com',
+  'http://localhost:5173',
+];
+
 app.use(cors({
-  origin: 'https://expense-tracker-fe.onrender.com', // Cho phép FE domain
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
